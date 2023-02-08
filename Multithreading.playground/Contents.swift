845a8254ecab397dec02f2be364789f92d9c8946
r.bigun@devlight.io <r.bigun@devlight.io>
2023-02-08 13:25:09 +0200

    -  DispatchQueue - WorkItems - URLSession

--- Multithreading.playground/Contents.swift
+++ Multithreading.playground/Contents.swift
@@ -308,3 +308,204 @@ class UnfairLock {
         os_unfair_lock_unlock(&lock)
     }
 }
+
+// High level sync
+class SynchronizationObjc {
+    private let lock = NSObject()
+    private var array = [Int]()
+    
+    func worker() {
+        objc_sync_enter(lock)
+        array.append(999)
+        objc_sync_exit(lock)
+    }
+}
+
+// MARK: - GCD -
+
+// We dont think about threads here We concentrate on tasks instead.
+// We have abstract QUEUES or LINES to work with. Threads created by iOs for us
+// We have serial & concurrent queues and tasks can be added sync(hronously) & asyn(chronously)
+
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+enum Queues {
+    
+    case serialCustom(String)
+    case cuncurentCustom(String)
+    case systemGlobal
+    case backgroundGlobal
+    case userInterative
+    case userInitiated
+    case utility
+    case main
+    
+    func get() -> DispatchQueue {
+        switch self {
+        case .utility:
+            return DispatchQueue.global(qos: .utility)
+        case .serialCustom(let value):
+            return DispatchQueue(label: value)
+        case .cuncurentCustom(let value):
+            return DispatchQueue(label: value, attributes: .concurrent)
+        case .systemGlobal:
+            return DispatchQueue.global()
+        case .backgroundGlobal:
+            return DispatchQueue.global(qos: .background)
+        case .userInterative:
+            return DispatchQueue.global(qos: .userInteractive)
+        case .userInitiated:
+            return DispatchQueue.global(qos: .userInitiated)
+        case .main:
+            return DispatchQueue.main
+        }
+    }
+    
+    // Before write a code we should decide:
+    // 1) Which queue we need to achieve needed result  - global or main (system or custom)
+    // 2) Which qos to choose (priority metters)
+    // 3) Which method we should use to put task into queue SYNC or ASYNC
+    
+    // Custom queues
+//    private let serialQueue = DispatchQueue(label: "serialQueueTest")
+//    private let cuncurrentQueue = DispatchQueue(label: "concurentQueueTest", attributes: .concurrent)
+//    // System queues
+//    // Global queue has a set of prirorities named QOS (Quality of service)
+//    private let systemGlobalQueue = DispatchQueue.global() // Default
+//    private let backgroundGlobalQueue = DispatchQueue.global(qos: .background) // very low level
+//    private let userInteractiveGlobalQueue = DispatchQueue.global(qos: .userInteractive) // Top level
+//    private let userInInitiatedGlobalQueue = DispatchQueue.global(qos: .userInitiated) // Very highg level
+//    private let utilityGlobalQueue = DispatchQueue.global(qos: .utility) // Highg level
+//
+//    // System main queue [UI/Gestures/Hit testing]
+//    private let systemSerialQueue = DispatchQueue.main
+    
+}
+
+//let serialCustomQueue = Queues.get(.serialCustom("my_serial_queue.com.Roman"))()
+//serialCustomQueue.sync {
+//    print("\(Thread.isMainThread) : \(Thread.current)")
+//}
+
+// true : <_NSMainThread: 0x600001dd8140>{number = 1, name = main}
+
+
+// MARK: - WORK ITEMS -
+
+class DispatchWorkItem1 {
+    private let queue = Queues.get(.cuncurentCustom("DispatchWorkItem1"))()
+    
+    func create() {
+        let workItem = DispatchWorkItem {
+            print("Started DispatchWorkItem1")
+            print(Thread.current)
+        }
+        
+        workItem.notify(queue: .main) {
+            print("Finished DispatchWorkItem1")
+            print(Thread.current)
+        }
+        queue.async(execute: workItem)
+    }
+}
+
+//let dispatchWorkItem1 = DispatchWorkItem1()
+//dispatchWorkItem1.create()
+
+/*
+ Started DispatchWorkItem1
+ <NSThread: 0x60000255ac00>{number = 15, name = (null)}
+ Finished DispatchWorkItem1
+ <_NSMainThread: 0x6000025501c0>{number = 1, name = main}
+ */
+
+class DispatchWorkItem2 {
+    private let queue = Queues.get(.serialCustom("DispatchWorkItem2"))()
+    
+    func create() {
+        queue.async {
+            sleep(1)
+            print("Task 1")
+            print(Thread.current)
+        }
+        queue.async {
+            sleep(1)
+            print("Task 2")
+            print(Thread.current)
+        }
+        
+        let workItem = DispatchWorkItem {
+            print("Started Work Item")
+            print(Thread.current)
+        }
+        
+        queue.async(execute: workItem)
+        
+        workItem.cancel() // Не работает если задача уже выполяется
+    }
+}
+
+//let dispatchWorkItem2 = DispatchWorkItem2()
+//dispatchWorkItem2.create()
+
+let imageURL = URL(string: "https://www.planetware.com/photos-large/USNY/usa-best-places-new-york.jpg")!
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 150))
+let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 300, height: 150))
+imageView.backgroundColor = UIColor.yellow
+imageView.contentMode = .scaleAspectFit
+view.addSubview(imageView)
+
+PlaygroundPage.current.liveView = view
+
+func fetchImage() {
+    let queue = Queues.get(.utility)()
+    queue.async {
+        if let data = try? Data(contentsOf: imageURL) {
+            Queues.get(.main)().async {
+                imageView.image = UIImage(data: data)
+            }
+        }
+    }
+}
+
+//fetchImage()
+
+// Dispatch work item image download
+
+func fetchImage2() {
+    var data: Data?
+    let queue = Queues.get(.userInitiated)()
+    let workItem = DispatchWorkItem(qos: .userInteractive) {
+        data = try? Data(contentsOf: imageURL)
+    }
+    
+    queue.async(execute: workItem)
+    
+    workItem.notify(queue: .main) {
+        if let imageData = data {
+            imageView.image = UIImage(data: imageData)
+        }
+    }
+    
+}
+
+//fetchImage2()
+
+// MARK: - URLSession
+
+func fetchImage3() {
+    let task = URLSession.shared.dataTask(with: imageURL) { data, response, error in
+        if let imageData = data {
+            Queues.get(.main)().async {
+                imageView.image = UIImage(data: imageData)
+                print("Image loaded")
+            }
+        }
+    }
+    task.resume()
+}
+
+//fetchImage3()
+
+
